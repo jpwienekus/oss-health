47c1af2a77de049e6d72ba84bd142e777e123669
JP Wienekus <jpwienekus@gmail.com>
2025-06-07 12:48:54 +0200

    remove unused mutation

--- web/src/graphql/mutations.ts
+++ web/src/graphql/mutations.ts
@@ -1,20 +1,5 @@
 import { gql } from 'graphql-request'
 
-export const SYNC_REPOS = gql`
-  mutation SyncRepositories {
-    syncRepositories {
-      repositories {
-        name
-        description
-        updatedAt
-        url
-        openIssues
-        score
-      }
-      syncDate
-    }
-  }
-`
 export const SAVE_SELECTED_REPOSITORIES = gql`
   mutation SaveSelectedRepositories($selectedGithubRepositoryIds: [Int!]!) {
     saveSelectedRepositories(
