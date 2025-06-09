7fd27d61ecef26bb32524c0156fa8b2caa0cd9c2
JP Wienekus <jpwienekus@gmail.com>
2025-06-09 11:25:04 +0200

    more lint fixes

--- api/app/parsers/__init__.py
+++ api/app/parsers/__init__.py
@@ -1,2 +1,4 @@
-from . import package_lock as package_lock, pnpm_lock as pnpm_lock, requirements_txt as requirements_txt
+from . import package_lock as package_lock
+from . import pnpm_lock as pnpm_lock
+from . import requirements_txt as requirements_txt
 from .base import dependency_parsers as dependency_parsers
