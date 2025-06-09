488622d1d07e7f0b750989e0b893afdd1f2c6bad
JP Wienekus <jpwienekus@gmail.com>
2025-06-09 11:19:27 +0200

    set up ruff

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -1,8 +1,7 @@
 from app.database import Base as Base
-
-from .dependency import Dependency
-from .dependency_vulnerability import DependencyVulnerability
-from .repository import Repository
-from .repository_dependency import RepositoryDependency
-from .user import User
-from .vulnerability import Vulnerability
+from .dependency import Dependency as Dependency
+from .dependency_vulnerability import DependencyVulnerability as DependencyVulnerability
+from .repository import Repository as Repository
+from .repository_dependency import RepositoryDependency as RepositoryDependency
+from .user import User as User
+from .vulnerability import Vulnerability as Vulnerability
