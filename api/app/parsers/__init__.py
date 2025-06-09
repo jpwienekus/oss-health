759e628909570051b37f1d03732ecf2d87d68e98
JP Wienekus <jpwienekus@gmail.com>
2025-06-09 11:02:16 +0200

    sort imports with isort

--- api/app/parsers/__init__.py
+++ api/app/parsers/__init__.py
@@ -1,4 +1,2 @@
-from . import package_lock as package_lock
-from . import pnpm_lock as pnpm_lock
-from . import requirements_txt as requirements_txt
-from .base import dependency_parsers as dependency_parsers
+from . import package_lock, pnpm_lock, requirements_txt
+from .base import dependency_parsers
