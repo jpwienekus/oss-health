7be818f848fe913e50f240baf2f1c1cff636f3b8
JP Wienekus <jpwienekus@gmail.com>
2025-06-17 13:58:27 +0200

    update tests to work with more than one db session test

--- api/tests/crud/test_repository.py
+++ api/tests/crud/test_repository.py
@@ -1,27 +1,46 @@
 import pytest
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.crud.user import add_user
 from app.models import Repository as RepositoryDBModel
-from app.crud.repository import get_repositories, add_repository_ids  # your module/functions
+from app.crud.repository import get_repositories, add_repository_ids
 
 @pytest.mark.asyncio
-async def test_add_repository_ids_and_get(db_session):
-    user_id = 1
-    tracked_repositories = [
-        {"id": 101, "clone_url": "https://github.com/user/repo1.git"},
-        {"id": 102, "clone_url": "https://github.com/user/repo2.git"},
+async def test_add_repository_ids(db_session: AsyncSession):
+    repos_to_add = [
+        {"id": 1111, "clone_url": "https://github.com/test/repo1"},
+        {"id": 2222, "clone_url": "https://github.com/test/repo2"},
     ]
 
-    await add_user(db_session=db_session, github_id=1, github_username="test_github_username", access_token="test_access_token")
-    # Add repos
-    await add_repository_ids(db_session=db_session, user_id=user_id, tracked_repositories=tracked_repositories)
+    user = await add_user(db_session=db_session, github_id=1, github_username="test_github_username", access_token="test_access_token")
+    await add_repository_ids(db_session, user.id, repos_to_add)
 
-    # Fetch repos back
-    repos = await get_repositories(db_session, user_id)
+    # Directly query to confirm they were added
+    result = await db_session.execute(
+        select(RepositoryDBModel).where(RepositoryDBModel.user_id == user.id)
+    )
+    repos = result.scalars().all()
 
     assert len(repos) == 2
-    assert repos[0].github_id == 101
-    assert repos[0].clone_url == "https://github.com/user/repo1.git"
-    assert repos[0].user_id == user_id
-    assert repos[1].github_id == 102
-    assert repos[1].clone_url == "https://github.com/user/repo2.git"
-    assert repos[1].user_id == user_id
+    assert {repo.github_id for repo in repos} == {1111, 2222}
+
+
+@pytest.mark.asyncio
+async def test_get_repositories(db_session: AsyncSession):
+    user = await add_user(db_session=db_session, github_id=1, github_username="test_github_username", access_token="test_access_token")
+    # Add test data manually
+    repo = RepositoryDBModel(
+        github_id=3333,
+        user_id=user.id,
+        score=0,
+        clone_url="https://github.com/test/repo3"
+    )
+    db_session.add(repo)
+    await db_session.commit()
+
+    # Now use the function to fetch it
+    repos = await get_repositories(db_session, user.id)
+
+    assert len(repos) == 1
+    assert repos[0].github_id == 3333
+    assert repos[0].clone_url == "https://github.com/test/repo3"
