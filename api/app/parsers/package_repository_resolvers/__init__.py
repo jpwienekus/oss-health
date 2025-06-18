5a43babbc86214b2e7fb6d0e5ddd2b4f19b76ccb
JP Wienekus <jpwienekus@gmail.com>
2025-06-18 12:24:22 +0200

    initial implementation of repository url resolvers

---
+++ api/app/parsers/package_repository_resolvers/__init__.py
@@ -0,0 +1,3 @@
+from . import pypi as pypi
+from . import npm as npm
+from .base import package_repository_resolvers as package_repository_resolvers
---
+++ api/app/parsers/package_repository_resolvers/base.py
@@ -0,0 +1,11 @@
+
+from typing import Awaitable, Callable, Optional, Dict
+
+package_repository_resolvers: Dict[str, Callable[[str], Awaitable[Optional[str]]]] = {}
+
+
+def register_package_repository_resolver(ecosystem: str):
+    def decorator(func: Callable[[str], Awaitable[Optional[str]]]):
+        package_repository_resolvers[ecosystem.lower()] = func
+        return func
+    return decorator
