3db48774dc4ef77d84f9d0da310267ae5c3fd940
JP Wienekus <jpwienekus@gmail.com>
2025-06-29 16:29:04 +0200

    fix migration (#59)

--- supabase/migrations/1751206862_migration.sql
+++ supabase/migrations/1751206862_migration.sql
@@ -1,15 +1,5 @@
 BEGIN;
 
--- Running upgrade 77abd35aa613 -> 1f0271d16689
-
-ALTER TABLE dependencies ADD COLUMN github_url_resolve_failed BOOLEAN DEFAULT false NOT NULL;
-
-ALTER TABLE dependencies ADD COLUMN github_url_resolve_failed_reason VARCHAR;
-
-CREATE INDEX ix_dependencies_github_url_resolve_failed ON dependencies (github_url_resolve_failed);
-
-UPDATE alembic_version SET version_num='1f0271d16689' WHERE alembic_version.version_num = '77abd35aa613';
-
 -- Running upgrade 1f0271d16689 -> 2a4b2566e2a9
 
 ALTER TABLE repositories ADD COLUMN scan_day INTEGER DEFAULT 0 NOT NULL;
